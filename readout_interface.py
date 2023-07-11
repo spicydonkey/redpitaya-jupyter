781991be12076a0a49867e19321c0a2224b5a65b
David Shin <david.shin@q-ctrl.com>
2023-07-11 10:19:11 +1000

    feat: create readout server and interface

---
+++ readout_interface.py
@@ -0,0 +1,78 @@
+"""Module for interfacing with the readout system.
+"""
+
+import logging
+
+import numpy as np
+import numpy.typing as npt
+import zmq
+
+
+logger = logging.getLogger(__name__)
+
+class ReadoutInterface:
+    def __init__(self, ip: str, port: str="5555") -> None:
+        self._ip = ip
+        self._port = port
+        self._context = zmq.Context()
+        self._socket = self._context.socket(zmq.REQ)
+        self._socket.connect(f"tcp://{self._ip}:{self._port}")
+        logger.info(f"Connected to tcp://{self._ip}:{self._port}")
+
+    def configure(self, 
+                  decimation: int,
+                  trigger_pre: int,
+                  trigger_post: int,
+                #   trig_src: str,
+                  ) -> None:
+        """Configure the readout system.
+        """
+        raise NotImplementedError
+        self._socket.send_pyobj({
+            "command": "configure",
+            "decimation": decimation,
+            "trigger_pre": trigger_pre,
+            "trigger_post": trigger_post,
+            # "trig_src": trig_src,
+        })
+        message = self._socket.recv_pyobj()
+        logger.info(f"Received reply: {message}")
+
+    def get_raw_buffer(self) -> tuple[npt.NDArray, int, float]:
+        """Get the raw buffer from the oscilloscope.
+
+        Returns:
+            (raw_samples, trigger, timestamp):
+                raw_samples: raw samples in buffer
+                trigger: index to trigger.
+                timestamp: timestamp of the trigger.
+        """
+        print("get_raw_buffer")
+        self._socket.send(b"1")
+        message = self._socket.recv(copy=False)
+        return message
+
+    def start(self):
+        raise NotImplementedError
+
+    def stop(self):
+        raise NotImplementedError
+
+def main(): 
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", type=str, help="ip address of the server",
+                        default="localhost")
+    parser.add_argument("-p", "--port", type=str, help="port of the server",
+                        default="5555")
+    parser.add_argument("-v", "--verbosity", action="count", default=0,
+                        help="increase output verbosity")
+    args = parser.parse_args()
+    print(f"{args = }")
+
+    readout_interface = ReadoutInterface(ip=args.ip, port=args.port)
+    # readout_interface.configure(decimation=1, trigger_pre=0, trigger_post=2**14)
+
+    while True:
+        message = readout_interface.get_raw_buffer()
+        print(f"{message = }")
---
+++ readout_server.py
@@ -0,0 +1,120 @@
+"""Module for the readout system server.
+"""
+
+import logging
+
+import numpy as np
+import zmq
+
+from redpitaya.overlay.mercury import mercury as overlay
+
+
+logger = logging.getLogger(__name__)
+
+class ReadoutServer:
+    """Readout system server.
+    """
+    def __init__(self, port: str="5555") -> None:
+        """Initialize the server.
+
+        Args:
+            port: The port to listen on. Defaults to "5555".
+        """
+        # Set up the FPGA
+        self._fpga = overlay()
+        # Set up the oscilloscope
+        self._osc0 = self._fpga.osc(0, 1.0)
+        # data rate decimation 
+        self._osc0.decimation = 4
+        # trigger timing [sample periods]
+        self._osc0.trigger_pre  = 0
+        self._osc0.trigger_post = self._osc0.buffer_size
+        # disable hardware trigger sources
+        self._osc0.trig_src = 0
+
+        # Set up the ZMQ socket
+        self._context = zmq.Context()
+        self._socket = self._context.socket(zmq.REP)
+        self._socket.bind(f"tcp://*:{port}")
+        print(f"Server started: tcp://*:{port}")
+
+    def configure_capture(self,
+                  decimation: int,
+                  trigger_pre: int,
+                  trigger_post: int,
+                #   trig_src: str,
+                  ) -> None:
+        """Configure the readout system.
+        """
+        raise NotImplementedError
+        self._osc0.decimation = decimation
+        self._osc0.trigger_pre = trigger_pre
+        self._osc0.trigger_post = trigger_post
+        
+        self._osc0.trig_src = 0  # disable hardware trigger sources
+        # self._osc0.trig_src = self._fpga.trig_src[trig_src]
+        # self._osc0.trig_src = self._fpga.trig_src["osc1"]
+        # self._osc0.edge = "pos"
+        # self._osc0.level = [0.4, 0.5]
+
+    def trigger(self) -> None:
+        """Force a trigger.
+        """
+        # synchronization and trigger sources are the default,
+        # which is the module itself
+        print("Triggering")
+        self._osc0.reset()
+        self._osc0.start()
+        self._osc0.trigger()
+
+    def run(self):
+        """Run the server.
+        """
+        print("Server running.")
+        while True:
+            
+            message = self._socket.recv()
+            # FIXME: currently there is no handling of requests -- the server just 
+            # sends the buffer back to the client.
+            print(f"Received request: {message}")
+            self.trigger()
+
+            # Bus error
+            #buffer_np = np.ctypeslib.as_array(self._osc0.buffer)
+            #socket.send(buffer_np, copy=True)  # error for even copy=False
+
+            # OK: 3 ms
+            #socket.send(np.arange(2**14), copy=False)
+
+            # OK: 2.5 ms
+            #socket.send(MOCK_DATA, copy=False)
+
+            # Crashes
+            #buffer_np = np.ctypeslib.as_array(self._osc0.buffer).copy()
+            #socket.send(buffer_np, copy=True)
+
+            # OK: 5.6 ms
+            buffer_np = np.ctypeslib.as_array(self._osc0.buffer)
+            buffer_copy = np.empty(len(self._osc0.buffer))
+            buffer_copy[:] = buffer_np[:]
+            self._socket.send(buffer_copy, copy=False)
+
+            # bus error
+            #buffer_np = np.ctypeslib.as_array(self._osc0.buffer)
+            #socket.send(buffer_np[:], copy=False)
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=str, help="port of the server",
+                        default="5555")
+    parser.add_argument("-v", "--verbosity", action="count", default=0,
+                        help="increase output verbosity")
+    args = parser.parse_args()
+    print(f"{args = }")
+
+    readout_server = ReadoutServer(args.port)
+    readout_server.run()
+
+if __name__ == "__main__":
+    main()
